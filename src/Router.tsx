35d4559a6cc63db0be0c39698740f399d3244521
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-04-02 14:59:02 -0300

    initial commit

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "home",
+    element: <Home />,
+  },
+]);
